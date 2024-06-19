452a0b4ef576bfc15c1f8775db02ee8b9874d381
AndreaVecchione <164344505+AndreaVecchione@users.noreply.github.com>
2024-06-20 00:24:28 +0200

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ import java.time.format.FormatStyle;
 
 public class Main {
     public static void main(String[] args) {
-
+//creazione oggetto
         OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        //inserisco la lunghezza della data in FULL per stampare tutti i campi richiesti dall'esercizio
         String dateFull  = date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL));
         System.out.println("Grandezza grande: " + dateFull);
 
